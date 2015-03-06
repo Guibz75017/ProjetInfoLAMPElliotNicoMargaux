294b4f8dd4e7ad1b3c633daa619bbc8e259c6ae8
Guibz75017 <guibe@ece.fr>
2015-03-06 11:39:07 +0100

    Mise a joue

--- index.php
+++ index.php
@@ -6,14 +6,36 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+      <meta charset="UTF-8">
+        <title>Le futur de l'énergie</title>
+        <link rel="stylesheet" type="text/css" href="./style.css" />
     </head>
     <body>
-        <h1> Bonjour </h1>
-        <h2> Nico est une petite salope.</h2>
-        <h3>bonjourrrrrr</h3>
-        <h4>mes couilles</h4>
+        <aside></aside>
+        <header>
+            <nav >
+                <ul id="barre">
+                    <li id="tete_menu"><a href="../Accueil/Accueil.php">Acceuil</a><ol>
+                            <li><a href="../Accueil/Accueil.php">Français</a></li>
+                            <li><a href="../Accueil/Home.php">English</a></li></ol></li>
+                            
+                    <li><a href="../Liste_des_sites/Liste_des_sites.php">Liste des sites</a><ol>
+                            <li><a href="../Liste_des_sites/Consulter_liste.php">Consulter liste</a></li>
+                            <li><a href="../Liste_des_sites/Ajouter_liste.php">Ajouter liste</a></li>
+                        </ol></li>
+                        
+                        <li><a href="../Détails_d_un_site/Détails_d_un_site.php">Détails d'un site</a><ol>
+                    <li><a href="../Détails_d_un_site/Sélection.php">Sélection</a></li>
+                    <li><a href="../Détails_d_un_site/Infos_du_site.php">Infos du site</a></li>
+                            </ol></li>
+
+                    <li id="queue_menu"><a href="../Voies_d_acheminement/Voies_d_acheminement.php">Voies d'acheminement</a><ol>
+                    <li><a href="../Voies_d_acheminement/Consulter_liste.php">Consulter liste</a></li>
+                    <li><a href="../Voies_d_acheminement/Ajouter_voie.php">Ajouter voie</a></li>
+                            </ol></li>
+                </ul>
+            </nav>
+        </header>
         <?php
         // put your code here
         ?>
